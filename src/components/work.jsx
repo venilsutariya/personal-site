84a9c3cfb1d6e250101041e3d052fcce1e5af42b
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2024-12-03 15:52:12 +0530

    add project

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -24,25 +24,25 @@ const Work = () => {
                     ))}
                 </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-[50px] gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-[50px] gap-4 lg:gap-0">
                 {projectList.map((project, index) => {
                     const filterArray = project.filter.split(",");
                     if (filter === "All" || filterArray.includes(filter)) {
                         return (
-                            <div key={index} className="px-4 lg:px-0 rounded-xl overflow-hidden">
+                            <div key={index} className="px-4 lg:px-0 overflow-hidden">
                                 <div
                                     className="custom-aspect-ratio cursor-pointer overflow-hidden z-50 relative group"
                                     onClick={() => router.push(project.href)}>
                                     <img
                                         src={project.imageUrl}
                                         alt={project.name}
-                                        className="object-cover hover:opacity-80 transition-all imageani cursor-pointer shadow-[#1e1d1d]"
+                                        className={`${project.name === 'Finance' ? "": "object-cover"} hover:opacity-80 transition-all imageani cursor-pointer shadow-[#1e1d1d]`}
                                     />
                                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-0 group-hover:h-full w-full hidden lg:flex justify-center items-center opacity-0 group-hover:opacity-100 bg-[#000]/50 backdrop-blur-lg transition-all">
                                         {project.name}
                                     </div>
                                 </div>
-                                <div className="py-3 flex flex-wrap gap-x-4">
+                                <div className="py-3 flex flex-wrap gap-x-4 lg:hidden">
                                     {project.view.includes("Github") && (
                                         <a data-cursor="-hidden" href={project?.githubUrl} target="_blank" className="py-1 px-3 rounded-full border border-white hover:bg-white hover:text-black">Github</a>
                                     )}
