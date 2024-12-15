e3fce48f62ed7589cab6409b8a1328a2022fe98e
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2024-12-15 14:37:49 +0530

    add new resources

--- src/lib/resourcesList.js
+++ src/lib/resourcesList.js
@@ -1,43 +1,13 @@
 export const resourcesList = [
   {
-    title: "NextAuth-2FA-Boilerplate",
-    image: "/images/next-auth.webp",
-    href: "https://github.com/venilsutariya/NextAuth-2FA-Boilerplate",
+    title: "3d Card Animation",
+    video: "/images/3dcardanimation.mp4",
+    href: "https://github.com/venilsutariya/3d-card-animation",
   },
   {
-    title: "Clerk Auth Boilerplate",
-    image: "/images/clerk.webp",
-    href: "https://github.com/venilsutariya/Clerk-authentication",
-  },
-  {
-    title: "Image Motion Trail",
-    video: "/images/motion-trail.mp4",
-    href: "https://github.com/venilsutariya/Image-Motion-Trail",
-  },
-  {
-    title: "Ripple Shader",
-    video: "/images/ripple-shader.mp4",
-    href: "https://github.com/venilsutariya/Ripple-Shader",
-  },
-  {
-    title: "Magnetic Button",
-    video: "/images/magnetic-button.mp4",
-    href: "https://github.com/venilsutariya/magnetic-button",
-  },
-  {
-    title: "Strach Rope Effect",
-    video: "/images/strach-rope.mp4",
-    href: "https://github.com/venilsutariya/Strach-Rope-Effect",
-  },
-  {
-    title: "Pixel Image Loading",
-    video: "/images/pixel-loading.mp4",
-    href: "https://github.com/venilsutariya/pixel-image-loading",
-  },
-  {
-    title: "Mouse Follower",
-    video: "/images/mouse-follower.mp4",
-    href: "https://github.com/venilsutariya/mouse-follower-demo",
+    title: "Parallax-Scroll",
+    video: "/images/parallax-scroll.mp4",
+    href: "https://github.com/venilsutariya/parallax-scroll",
   },
   {
     title: "3D Image Sphere",
@@ -45,8 +15,43 @@ export const resourcesList = [
     href: "https://github.com/venilsutariya/3D-Image-Sphere",
   },
   {
-    title: "Parallax-Scroll",
-    video: "/images/parallax-scroll.mp4",
-    href: "https://github.com/venilsutariya/parallax-scroll",
+    title: "Mouse Follower",
+    video: "/images/mouse-follower.mp4",
+    href: "https://github.com/venilsutariya/mouse-follower-demo",
+  },
+  {
+    title: "Pixel Image Loading",
+    video: "/images/pixel-loading.mp4",
+    href: "https://github.com/venilsutariya/pixel-image-loading",
+  },
+  {
+    title: "Strach Rope Effect",
+    video: "/images/strach-rope.mp4",
+    href: "https://github.com/venilsutariya/Strach-Rope-Effect",
+  },
+  {
+    title: "Magnetic Button",
+    video: "/images/magnetic-button.mp4",
+    href: "https://github.com/venilsutariya/magnetic-button",
+  },
+  {
+    title: "Ripple Shader",
+    video: "/images/ripple-shader.mp4",
+    href: "https://github.com/venilsutariya/Ripple-Shader",
+  },
+  {
+    title: "Image Motion Trail",
+    video: "/images/motion-trail.mp4",
+    href: "https://github.com/venilsutariya/Image-Motion-Trail",
+  },
+  {
+    title: "Clerk Auth Boilerplate",
+    image: "/images/clerk.webp",
+    href: "https://github.com/venilsutariya/Clerk-authentication",
+  },
+  {
+    title: "NextAuth-2FA-Boilerplate",
+    image: "/images/next-auth.webp",
+    href: "https://github.com/venilsutariya/NextAuth-2FA-Boilerplate",
   },
 ];
