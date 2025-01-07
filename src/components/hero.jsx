107d13c3d208d825e8f72e97274a0ffcf043e004
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2025-01-07 11:29:16 +0530

    add book an appoitment button

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,8 @@
 import WordRotate from "@/components/word-rotate";
+import Link from "next/link";
 
 const Hero = () => {
+
     return (
         <div
             className={`px-10 py-5 lg:px-20 lg:py-10`}>
@@ -14,6 +16,19 @@ const Hero = () => {
             <p className="lg:ps-40 text-lg lg:text-xl mt-10 lg:max-w-[600px]">
                 I &apos; m a full-stack developer, loves building products and designing experiences.
             </p>
+            <div className="lg:ps-40 mt-5">
+                <Link
+                    href={"https://calendly.com/venilsutariya7/30min"}
+                    target="_blank"
+                >
+                    <button
+                        data-cursor="-hidden"
+                        className="mt-[24px] rounded-full py-2 px-3 shadow-sm shadow-white/20 flex items-center gap-x-4 bg-gradient-to-tr from-blue-400/30 via-transparent to-violet-400/30 hover:from-violet-400/30 hover:to-blue-400/30 hover:via-transparent"
+                    >
+                        Book an appointment
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 }
